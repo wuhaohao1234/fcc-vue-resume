dcc2a422c24d50e3dcacf6cdad3011574b2daaa1
wuhaohao1234 <1611499758@qq.com>
2020-04-30 14:46:14 +0800

    init: vue

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # fcc-vue-resume
-这是一道面试题的题目
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,18 @@
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+describe('这是一个简单的单元测试', () => {
+  it('1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
